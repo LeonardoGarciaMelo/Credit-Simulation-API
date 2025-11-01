bbf8e8c57e8c32f55f3594afa805290715df2b2e
Leonardo Garcia Melo <lgarciamelo213@gmail.com>
2025-11-01 11:57:29 -0300

    Creating RegistrosDTO

---
+++ Simulador_de_Credito/DTO/RegistrosDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Simulador_de_Credito.DTO
+    ///<summary>
+    /// Representa o registro de uma simulação 
+    /// </summary>
+    /// <remarks>
+    /// Este objeto acessa o banco de dados simulacoes e pega os dados necessários, para retornar no endpoint de listar todas as simulacoes
+    /// </remarks>
+{
+    public record RegistrosDTO
+    {
+        [JsonPropertyName("idSimulacao")]
+        public int Id { get; init; }
+
+        [JsonPropertyName("valorDesejado")]
+        public decimal ValorDesejado { get; init; }
+
+        [JsonPropertyName("prazo")]
+        public short Prazo { get; init; }
+
+        [JsonPropertyName("valorTotalParcelas")]
+        public decimal ValorTotalParcelas { get; init; }
+    }
+}
